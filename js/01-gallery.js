dc14b35ec52c3a4b010c1658c3f5035c3467d74d
Bartek Świderski <bartekzg@gmail.com>
2021-09-07 16:10:37 +0200

    ok

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import _default from "../node_modules/simplelightbox/dist/simple-lightbox.esm";
+import "../node_modules/simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const qs = (selector) => document.querySelector(selector);
+const galleryGrid = qs(".gallery")
+const prev = galleryItems.map(prev=>prev.preview);
+const oryg = galleryItems.map(org=>org.original);
+const desc = galleryItems.map(des=>des.description);
+let galleryMarkup = "";
+for (let i = 0; i<prev.length; i++){
+galleryMarkup+=`<a class="gallery__item" href="${oryg[i]}">
+<img class="gallery__image" src="${prev[i]}" alt="${desc[i]}"  />
+</a>`
+}
+galleryGrid.insertAdjacentHTML("beforeend", galleryMarkup);
+let gallery = new SimpleLightbox('.gallery a',{captionsData:"alt", captionDelay:"250"});
+gallery.on('show.simplelightbox', function () {
+	preventDefault()
+});
+galleryGrid.addEventListener("click", preventDefault())
