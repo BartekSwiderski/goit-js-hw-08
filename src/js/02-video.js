8b5c4ae8ae55442499f960ce886c87815f4c508f
Bartek Świderski <bartekzg@gmail.com>
2021-09-07 21:48:11 +0200

    poprawki

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
-
+import _default from "../../node_modules/@vimeo/player/dist/player";
+import throttle from '../../node_modules/lodash/throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let playerTime = function(data)  {
+  localStorage.setItem("videoplayer-current-time",data.seconds);
+}
+player.on('timeupdate',throttle (playerTime, 1000));
+const time = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(time).then(function(seconds) {
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+      default:
+          break;
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from '../../node_modules/lodash/throttle';
 
+const form = document.querySelector(".feedback-form");
+
+function formInput(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+  let object = {email:`${email.value}`, message:`${message.value}`}
+  localStorage.setItem("feedback-form-state",JSON.stringify(object))
+}
+form.addEventListener("input", throttle(formInput,500));
+const savedSettings = localStorage.getItem("feedback-form-state")
+try {
+  let parsedSettings = JSON.parse(savedSettings);
+  form.email.value = parsedSettings.email;
+  form.message.value = parsedSettings.message;
+} catch (error) {
+  console.log(`Błąd ${error.name}`)
+}
+function formSubmit(event){
+  event.preventDefault();
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+  let object = {email:`${email.value}`, message:`${message.value}`}
+  console.log(object)
+  localStorage.clear();
+  event.currentTarget.reset();
+}
+form.addEventListener("submit", formSubmit);
