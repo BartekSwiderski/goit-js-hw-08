f199719a44e0022fef57efcf42ac7904ce459486
Bartek Świderski <bartekzg@gmail.com>
2021-09-07 16:27:26 +0200

    ok

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
-import _default from "../node_modules/simplelightbox/dist/simple-lightbox.esm";
+import _default from "../node_modules/simplelightbox/dist/simple-lightbox";
 import "../node_modules/simplelightbox/dist/simple-lightbox.min.css";
+
+
+
 // Change code below this line
 
 
